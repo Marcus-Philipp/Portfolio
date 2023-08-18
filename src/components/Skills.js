903aa9c02dab66821e70691b399eb1e46e84f5a4
Marcus-Philipp <philippmarcus89@gmail.com>
2023-08-18 07:40:19 +0200

    JSON und TypeScript hinzugefuegt

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,10 +16,12 @@ const Skills = () => {
                     <div className='w-3/5'>
                         <SkillItem iconClass='bx bxl-html5' text='HTML5' color='text-orange-500' number='85' />
                         <SkillItem iconClass='bx bxl-css3' text='CSS3' color='text-blue-500' number='80' />
-                        <SkillItem iconClass='bx bxl-javascript' text='JavaScript' color='text-yellow-400' number='66' />
+                        <SkillItem iconClass='bx bxl-javascript' text='JavaScript' color='text-yellow-400' number='70' />
                         <SkillItem iconClass='bx bxl-jquery' text='jQuery' color='text-blue-400' number='78' />
+                        <SkillItem iconClass='bx bxl-typescript' text='TypeScript' color='text-blue-400' number='66' />
                         <SkillItem iconClass='bx bxl-react' text='React' color='text-blue-300' number='63' />
                         <SkillItem iconClass='bx bxl-react' text='React Native' color='text-blue-300' number='60' />
+                        <SkillItem iconClass='bx bxs-file-json' text='JSON & API' color='text-black' number='69' />
                         <SkillItem iconClass='bx bxl-bootstrap' text='Bootstrap' color='text-purple-500' number='45' />
                         <SkillItem iconClass='bx bxl-tailwind-css' text='Tailwind CSS' color='text-blue-400' number='55' />
                         <SkillItem iconClass='bx bxs-devices' text='Responsive Web Design' color='text-black-500' number='70' />
